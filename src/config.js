795213c64f63e9b328de8d0a2e07203b9d4b769c
xiangzhoumeng <xiangzhoumeng@kugou.net>
2016-07-22 17:19:44 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const API_ROOT = (process.env.NODE_ENV === 'production')
+			? 'http://api.jackhu.top/'
+			:'http://api.jackhu.top/'
+
+export const CookieDomain = (process.env.NODE_ENV === 'production')
+			? '.jackhu.top'
+			:''
---
+++ webpack.server.js
@@ -0,0 +1,30 @@
+var Express = require('express');
+var webpack = require('webpack');
+
+var webpackConfig = require('./webpack.config.js');
+var compiler = webpack(webpackConfig);
+var port = 3000;
+var serverOptions = {
+    contentBase: 'http://' + 'localhost' + ':' + port,
+    quiet: true,
+    noInfo: false,
+    hot: true,
+    inline: true,
+    lazy: false,
+    publicPath: webpackConfig.output.publicPath,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    stats: {colors: true}
+};
+
+var app = new Express();
+
+app.use(require('webpack-dev-middleware')(compiler, serverOptions));
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.listen(port, function onAppListening(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.info('==> 🚧  Webpack development server listening on port %s', port);
+    }
+});
