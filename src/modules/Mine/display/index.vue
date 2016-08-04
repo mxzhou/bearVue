e201e2d7d352efe537b25902d410c7009cf9d584
xiangzhoumeng <xiangzhoumeng@kugou.net>
2016-08-04 18:38:33 +0800

    晒单-待晒单

--- src/modules/Mine/display/index.vue
+++ src/modules/Mine/display/index.vue
@@ -4,15 +4,35 @@
 <template>
   <div>
     <tab class="fix">
-      <tab-item :selected="demo1 === '待晒单'" @click="demo1 = '待晒单'"><span>待晒单</span></tab-item>
-      <tab-item :selected="demo1 === '已晒单'" @click="demo1 = '已晒单'">已晒单</tab-item>
+      <tab-item :selected="title === '待晒单'" @click="getList(0)"><span>待晒单</span></tab-item>
+      <tab-item :selected="title === '已晒单'" @click="getList(1)">已晒单</tab-item>
     </tab>
+    <ul class="list">
+      <template v-for="item in items">
+        <li>
+          <div class="left">
+            <img class="img" :src="item.coverImgUrl">
+          </div>
+          <div class="right">
+            <div class="desc short">
+              <p class="name">{{item.goodsName}}</p>
+              <p class="p">期号：{{item.id}}</p>
+              <div class="p clearfix">总需：{{item.needNumber}}
+              </div>
+            </div>
+            <a class="add" @click="addFunc">晒单</a>
+
+          </div>
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
   import { Tab, TabItem,Loading } from '../../../components'
   import {changeTitle} from '../../../utils/hack'
+  import {getSnarchRecordList} from '../../../vuex/actions'
 
 
   export default {
@@ -21,14 +41,38 @@
     },
     data () {
       return {
-        demo1: '待晒单',
+        title: '待晒单',
+        titleList:['待晒单','已晒单']
       }
     },
     init() {
-      changeTitle('我的夺宝')
+      changeTitle('晒单')
+    },
+    vuex: {
+      getters: {
+        items: function(store){
+          return store.snarchRecord.items
+        }
+      },
+      actions: {
+        getSnarchRecordList
+      }
+    },
+    created(){
+      this.getSnarchRecordList();
     },
     methods: {
+      addFunc:function(){
+        alert('晒单')
+      },
+      getList:function(index){
+        this.title = this.titleList[index];
+        if(index == 0){
 
+        }else if(index == 1){
+
+        }
+      }
     }
   }
 
