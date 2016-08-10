e2ec2d3dbebe1bd1f3c1399213d16705e2f7d2fc
jhonhuang <huangjianzhou@kugou.net>
2016-08-10 18:34:54 +0800

    商品详情状态

--- src/modules/Home/detail/index.vue
+++ src/modules/Home/detail/index.vue
@@ -4,10 +4,10 @@
         <Swiper :list="goodsDetail.goodsImgList" :loop="true" :height="adHeight" :theme="theme" :top="adTop"></Swiper>
         <!-- 商品信息 satrt -->
         <div class="info">
-            <h3><span class="status green">进行中</span>iPhone6s Plus 5.5英寸 128G 国行正品Phone6s Plus 5.5英寸 128G 国行正品</h3>
+            <h3><span class="status green">进行中</span><span class="status blue">已揭晓</span><span class="status gray">已下架</span>{{goodsDetail.goodsName}}</h3>
             <p>狂酷拽霸叼炸天的手机，秒杀安卓WP，装逼实用一样不落，新时代人类的首选</p>
         </div>
-        <div class="status-bar">
+        <div class="status-bar" style="display:none;">
             期号：2394024923424
             <div class="progress-bar">
               <div class="inner"></div>
@@ -15,9 +15,35 @@
             <div class="fr">剩余: <em>2333</em></div>
             <div class="fl">总需: 10086</div>
         </div>
-        <div class="status-bar" style="display:none;">
+        <div class="status-bar" style="height:0.9rem;display:none;">
             <div class="countdown">
-                
+                <a class="fr btn">计算详情</a>
+                <p>期号: 301221284</p>
+                <h3>揭晓倒计时 00:08:13</h3>
+            </div>
+        </div>
+        <div class="status-bar" style="height:0.9rem;">
+            <div class="removed">
+                奖品已下架
+            </div>
+        </div>
+        <div class="status-block" style="display:none;">
+            <div class="winner-block">
+                <div class="inner">
+                    <div class="user-pic">
+                        <img src="../../../assets/images/img_cartempty.png">
+                    </div>
+                    <h4>获奖者：<span class="winnerName">是第三方的</span></h4>
+                    <p style="color:#AAA;">（中国广东 IP：12423424234）</p>
+                    <p>用户ID: 2434234</p>
+                    <p>本期参与: <span class="red">234432</span> 人次</p>
+                    <p>本期期号: 2434234</p>
+                    <p>揭晓时间: 2434234</p>
+                </div> 
+                <div class="lucky-num">
+                    <a class="fr">计算详情&gt;</a>
+                    幸运号码：10004435
+                </div> 
             </div>
         </div>
         <div class="join">
