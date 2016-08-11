ab03e7260360fffe1ab4e25ad108d878c6e48012
xiangzhoumeng <xiangzhoumeng@kugou.net>
2016-08-11 09:22:58 +0800

    vue 文件 <script type="text/ecmascript-6">

--- src/modules/Mine/Recharge/record.vue
+++ src/modules/Mine/Recharge/record.vue
@@ -20,7 +20,7 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import { Loading, Group, Cell } from '../../../components'
   import {getRechargeRecordList} from '../../../vuex/actions'
   import {changeTitle} from '../../../utils/hack'
